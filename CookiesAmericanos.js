a03de8c01bcd1de6dd3bf4d3b8e04f092e1352c6
Leonardo B Brentano <95104325+leobbt@users.noreply.github.com>
2022-02-11 15:52:08 -0300

    Add files via upload

---
+++ CookiesAmericanos.js
@@ -0,0 +1,18 @@
+//                                          Receita de Cokies
+
+//  ingredientes:
+// 2 tebletes de margarina/ manteiga ou 200g de manteiga
+// 1 xícara de açúcar branco
+// 1 xícara de açúcar mascavo
+// 1 colher de sopa de essência de baunilha
+// 2 ovos
+// 3 xícaras de farinha
+// 1 colher de sopa de fermento
+// 1/2 colher de sopa de sal
+// 2 xícaras de gotas de chocolate
+
+//  preparo:
+// coloque na batedeira a manteiga a temperatura ambiente, os açúcares e bater.
+// acrescente os ovos e essência e bater. depois colocar sal, fermento, farinha e bater.
+// por último acrecente as gotas de chocolate.
+// faça bolinhas e coloque na assadeira e assar por no máximo 1o minutos.
